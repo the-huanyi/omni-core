f8c443aef97c4350159daa42d5c5d9f39a4c7f66
MueShen <mueshen.sh@gmail.com>
2025-09-04 08:18:56 +0300

    window created, vulkan extensions number checked

--- src/main.c
+++ src/main.c
@@ -1,5 +1,31 @@
-#include "stdio.h"
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
 
-void main() {
-	printf("hi");
+#define GLM_FORCE_RADIANS
+#define CGLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <cglm/cglm.h> //math
+
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+	glfwInit();
+	
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window",nullptr, nullptr);
+	
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,nullptr);
+	
+	printf("%d\n", extensionCount);
+	
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
